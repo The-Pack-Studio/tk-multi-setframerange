fd26f94887f35c8b03521123a9d77016efe086ea
Donat Van Bellinghen <donat.vanb@yahoo.com>
2014-10-16 21:45:16 +0200

    changes from Gael

--- app.py
+++ app.py
@@ -42,22 +42,22 @@ class SetFrameRange(Application):
         Callback from when the menu is clicked.
         """
 
-        (new_in, new_out) = self.get_frame_range_from_shotgun()
-        (current_in, current_out) = self.get_current_frame_range(self.engine.name)
+        (new_in, new_out, new_range_in, new_range_out) = self.get_frame_range_from_shotgun()
+        (current_in, current_out, current_range_in, current_range_out) = self.get_current_frame_range(self.engine.name)
 
         if new_in is None or new_out is None:
             message =  "Shotgun has not yet been populated with \n"
             message += "in and out frame data for this Shot."
 
-        elif int(new_in) != int(current_in) or int(new_out) != int(current_out):
+        elif int(new_in) != int(current_in) or int(new_out) != int(current_out) or int(new_range_in) != int(current_range_in) or  int(new_range_out) != int(current_range_out):
             # change!
             message =  "Your scene has been updated with the \n"
             message += "latest frame ranges from shotgun.\n\n"
-            message += "Previous start frame: %d\n" % current_in
-            message += "New start frame: %d\n\n" % new_in
-            message += "Previous end frame: %d\n" % current_out
-            message += "New end frame: %d\n\n" % new_out
-            self.set_frame_range(self.engine.name, new_in, new_out)
+            message += "Previous start frame: %d (handle: %d)\n" % (current_in, current_range_in)
+            message += "New start frame: %d (handle: %d)\n\n" % (new_in, new_range_in)
+            message += "Previous end frame: %d (handle: %d)\n" % (current_out, current_range_out)
+            message += "New end frame: %d (handle: %d)\n\n" % (new_out, new_range_out)
+            self.set_frame_range(self.engine.name, new_in, new_out, new_range_in, new_range_out)
 
         else:
             # no change
@@ -94,7 +94,8 @@ class SetFrameRange(Application):
 
         sg_in_field = self.get_setting("sg_in_frame_field")
         sg_out_field = self.get_setting("sg_out_frame_field")
-        fields = [sg_in_field, sg_out_field]
+
+        fields = [sg_in_field, sg_out_field, "sg_head_in", "sg_tail_out"]
 
         data = self.shotgun.find_one(sg_entity_type, filters=sg_filters, fields=fields)
 
@@ -109,7 +110,17 @@ class SetFrameRange(Application):
                                  "%s. This entity type does not have a "
                                  "field %s.%s!" % (sg_entity_type, sg_entity_type, sg_out_field))
 
-        return ( data[sg_in_field], data[sg_out_field] )
+        if "sg_head_in" not in data:
+            raise tank.TankError("Configuration error: Your current context is connected to a Shotgun "
+                                 "%s. This entity type does not have a "
+                                 "field %s.%s!" % (sg_entity_type, sg_entity_type, "sg_head_in"))
+
+        if "sg_tail_out" not in data:
+            raise tank.TankError("Configuration error: Your current context is connected to a Shotgun "
+                                 "%s. This entity type does not have a "
+                                 "field %s.%s!" % (sg_entity_type, sg_entity_type, "sg_tail_out"))            
+
+        return ( data[sg_in_field], data[sg_out_field],  data["sg_head_in"], data["sg_tail_out"])
 
 
     def get_current_frame_range(self, engine):
@@ -119,36 +130,52 @@ class SetFrameRange(Application):
             import maya.cmds as cmds
             current_in = cmds.playbackOptions(query=True, minTime=True)
             current_out = cmds.playbackOptions(query=True, maxTime=True)
-
+            range_in = cmds.playbackOptions(query=True, animationStartTime=True)
+            range_out = cmds.playbackOptions(query=True, animationEndTime=True)
         elif engine == "tk-nuke":
             import nuke
-            current_in = nuke.root()["first_frame"].value()
-            current_out = nuke.root()["last_frame"].value()
+            range_in = nuke.root()["first_frame"].value()
+            range_out = nuke.root()["last_frame"].value()
+            Viewer = nuke.activeViewer().node()
+            if Viewer: 
+                val = Viewer['frame_range'].value()
+                if val != '':
+                    current_in, current_out = val.split("-") 
+                    current_in = int(current_in)
+                    current_out = int(current_out)
+                else:
+                    current_in = range_in
+                    current_out = range_out
 
-        elif engine == "tk-motionbuilder":
-            from pyfbsdk import FBPlayerControl, FBTime
+            else:
+                current_in = range_in
+                current_out = range_out
 
-            lPlayer = FBPlayerControl()
-            current_in = lPlayer.LoopStart.GetFrame()
-            current_out = lPlayer.LoopStop.GetFrame()
 
-        elif engine == "tk-softimage":
-            import win32com
-            xsi = win32com.client.Dispatch('XSI.Application')
+        # elif engine == "tk-motionbuilder":
+        #     from pyfbsdk import FBPlayerControl, FBTime
 
-            current_in = xsi.GetValue("PlayControl.In")
-            current_out = xsi.GetValue("PlayControl.Out")
+        #     lPlayer = FBPlayerControl()
+        #     current_in = lPlayer.LoopStart.GetFrame()
+        #     current_out = lPlayer.LoopStop.GetFrame()
 
-        elif engine == "tk-houdini":
-            import hou
-            current_in, current_out = hou.playbar.playbackRange()
+        # elif engine == "tk-softimage":
+        #     import win32com
+        #     xsi = win32com.client.Dispatch('XSI.Application')
+
+        #     current_in = xsi.GetValue("PlayControl.In")
+        #     current_out = xsi.GetValue("PlayControl.Out")
+
+        # elif engine == "tk-houdini":
+        #     import hou
+        #     current_in, current_out = hou.playbar.playbackRange()
 
         else:
             raise tank.TankError("Don't know how to get current frame range for engine %s!" % engine)
 
-        return (current_in, current_out)
+        return (current_in, current_out, range_in, range_out)
 
-    def set_frame_range(self, engine, in_frame, out_frame):
+    def set_frame_range(self, engine, in_frame, out_frame, in_range, out_range):
 
         if engine == "tk-maya":
             import pymel.core as pm
@@ -156,8 +183,8 @@ class SetFrameRange(Application):
             # set frame ranges for plackback
             pm.playbackOptions(minTime=in_frame, 
                                maxTime=out_frame,
-                               animationStartTime=in_frame,
-                               animationEndTime=out_frame)
+                               animationStartTime=in_range,
+                               animationEndTime=out_range)
            
             # set frame ranges for rendering
             defaultRenderGlobals=pm.PyNode('defaultRenderGlobals')
@@ -172,29 +199,34 @@ class SetFrameRange(Application):
             if locked:
                 nuke.root()["lock_range"].setValue(False)
             # set values
-            nuke.root()["first_frame"].setValue(in_frame)
-            nuke.root()["last_frame"].setValue(out_frame)
+            nuke.root()["first_frame"].setValue(in_range)
+            nuke.root()["last_frame"].setValue(out_range)
+
+            for n in nuke.allNodes('Viewer'):
+                n['frame_range_lock'].setValue(False)
+                n['frame_range'].setValue('%i-%i' % (int(in_frame), int(out_frame)))
+                n['frame_range_lock'].setValue(True)
             # and lock again
-            if locked:
-                nuke.root()["lock_range"].setValue(True)
+            #if locked:
+            nuke.root()["lock_range"].setValue(True)
 
-        elif engine == "tk-motionbuilder":
-            from pyfbsdk import FBPlayerControl, FBTime
+        # elif engine == "tk-motionbuilder":
+        #     from pyfbsdk import FBPlayerControl, FBTime
 
-            lPlayer = FBPlayerControl()
-            lPlayer.LoopStart = FBTime(0, 0, 0, in_frame)
-            lPlayer.LoopStop = FBTime(0, 0, 0, out_frame)
+        #     lPlayer = FBPlayerControl()
+        #     lPlayer.LoopStart = FBTime(0, 0, 0, in_frame)
+        #     lPlayer.LoopStop = FBTime(0, 0, 0, out_frame)
 
-        elif engine == "tk-softimage":
-            import win32com
-            Application = win32com.client.Dispatch('XSI.Application')
+        # elif engine == "tk-softimage":
+        #     import win32com
+        #     Application = win32com.client.Dispatch('XSI.Application')
 
-            Application.SetValue("PlayControl.In", in_frame)
-            Application.SetValue("PlayControl.Out", out_frame)
+        #     Application.SetValue("PlayControl.In", in_frame)
+        #     Application.SetValue("PlayControl.Out", out_frame)
 
-        elif engine == "tk-houdini":
-            import hou
-            hou.playbar.setPlaybackRange(in_frame, out_frame)
+        # elif engine == "tk-houdini":
+        #     import hou
+        #     hou.playbar.setPlaybackRange(in_frame, out_frame)
 
         else:
             raise tank.TankError("Don't know how to set current frame range for engine %s!" % engine)
